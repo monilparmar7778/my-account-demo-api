5aea09cb6012cfbb9ef3cc37aff759bb00280830
Monilparmar7778 <monilparmar77@gmail.com>
2025-11-15 13:30:27 +0530

    chnges employee details module emplooe modal

--- my-account-api/models/Employee.cs
+++ my-account-api/models/Employee.cs
@@ -3,7 +3,7 @@
 	public class Employee
 	{
 		public long emp_details_id { get; set; }
-		public long employee_id { get; set; } 
+		public long? employee_id { get; set; } 
 		public decimal employee_amount { get; set; }
 		public string employee_descripation { get; set; }
 		public DateTime? insert_date { get; set; }
